4cfd309c9c02edc92b4c7b08e2c7592a40866c40
OneZeroBeatz <onezerobeatz@gmail.com>
2017-06-23 16:37:25 +0200

    Mrdzovanje

--- OsmiSemestarProj/src/main/java/xws_pi_bezb/helpers/Strings.java
+++ OsmiSemestarProj/src/main/java/xws_pi_bezb/helpers/Strings.java
@@ -1,8 +1,8 @@
-package xws_pi_bezb.helpers;
-
-public class Strings {
-
-	public static final String fizickoLice = "FizickoLice";
-	public static final String pravnoLice = "PravnoLice";
-	public static final String menadzerSistema = "MenadzerSistema";
-}
+//package xws_pi_bezb.helpers;
+//
+//public class Strings {
+//
+//	public static final String fizickoLice = "FizickoLice";
+//	public static final String pravnoLice = "PravnoLice";
+//	public static final String menadzerSistema = "MenadzerSistema";
+//}
